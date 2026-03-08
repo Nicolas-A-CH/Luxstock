0363023192edc3755eb2f0cf654acdcd1531c188
Luis Nicolas Arevalo Chiquiza <nicolasarevalochiquiza@gmail.com>
2026-03-07 20:07:34 -0500

    first commit

---
+++ src/main/java/com/luxStock/demo/LuxStockApplication.java
@@ -0,0 +1,13 @@
+package com.luxStock.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luxStock/demo/LuxStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxStock.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
